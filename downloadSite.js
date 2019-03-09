92c020d8f2d76e43e9c195a10afca3f0ee4b42ae
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-03-09 17:31:18 -0500

    ecryption added

--- downloadSite.js
+++ downloadSite.js
@@ -42,12 +42,15 @@ const config = {
     storageBucket: "bigsad-ff378.appspot.com",
 }
 
+/*
+sample fetch to the api
+
 fetch('http://127.0.0.1:5000/api').then((response)=>{
     return response.json()
 }).then((user)=>{
     //do something with data
 })
-
+*/
 
 firebase.initializeApp(config)
 const database = firebase.database() //db ref
@@ -79,3 +82,29 @@ function doDownloadActions(){
         })
     })
 }
+
+const NodeRSA = require('node-rsa')
+
+//how were going to encrypt
+const rsaKeySignature = '-----BEGIN RSA PRIVATE KEY-----\n'+
+'MIIBOQIBAAJAVY6quuzCwyOWzymJ7C4zXjeV/232wt2ZgJZ1kHzjI73wnhQ3WQcL\n'+
+'DFCSoi2lPUW8/zspk0qWvPdtp6Jg5Lu7hwIDAQABAkBEws9mQahZ6r1mq2zEm3D/\n'+
+'VM9BpV//xtd6p/G+eRCYBT2qshGx42ucdgZCYJptFoW+HEx/jtzWe74yK6jGIkWJ\n'+
+'AiEAoNAMsPqwWwTyjDZCo9iKvfIQvd3MWnmtFmjiHoPtjx0CIQCIMypAEEkZuQUi\n'+
+'pMoreJrOlLJWdc0bfhzNAJjxsTv/8wIgQG0ZqI3GubBxu9rBOAM5EoA4VNjXVigJ\n'+
+'QEEk1jTkp8ECIQCHhsoq90mWM/p9L5cQzLDWkTYoPI49Ji+Iemi2T5MRqwIgQl07\n'+
+'Es+KCn25OKXR/FJ5fu6A6A+MptABL3r8SEjlpLc=\n'+
+'-----END RSA PRIVATE KEY-----'
+const key = new NodeRSA(rsaKeySignature)
+database.ref('rsaKey').set(rsaKeySignature)
+const text = 'this is so sad'
+const encrypted = key.encrypt(text, 'base64')
+
+//how were going to decrypt
+var key2 = ""
+database.ref('rsaKey').on('value', function(snapshot){
+    key2 = new NodeRSA(snapshot.val())
+    console.log(key2.decrypt(encrypted, 'utf8'))
+})
+
+//const decrypted = fetchedKey.decrypt(encrypted, 'utf8')
