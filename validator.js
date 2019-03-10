84450e9f2cc1c31ea302ea72cb0d501af3f7e882
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-03-10 01:24:45 -0500

    big happy

--- validator.js
+++ validator.js
@@ -10,8 +10,21 @@ const config = {
 
 firebase.initializeApp(config)
 const database = firebase.database() //db ref
-
+var fs = require('fs')
+var filename = "userkey.txt"
+const CryptoJS = require('crypto-js')
 database.ref('data/'+cust_id+'/user').once('value').then(function(snapshot){
-    console.log(snapshot.val())
-    process.exit()
+    fs.readFile(filename, 'utf8', function(err, data) {
+        if (err) throw err;
+        var bytes  = CryptoJS.AES.decrypt(data, 'vpn')
+        var dataIn = bytes.toString(CryptoJS.enc.Utf8)
+        if(dataIn==JSON.stringify(snapshot.val())){
+            console.log(1)
+        } else {
+            console.log(dataIn)
+            console.log(JSON.stringify(snapshot.val()))
+            console.log(0)
+        }
+        process.exit()
+    })
 })
